3369b6ebe02946d6b6e455aa488d780eeb3ffdcc
gunikrengkidesu <chanunchidat6@gmail.com>
2026-02-22 21:34:37 +0700

    เปลี่ยนชื่อ folder

---
+++ emoo/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { K2D } from "next/font/google";
+import "./globals.css";
+
+const k2d = K2D({
+  subsets: ["latin", "thai"],
+  weight: ["100","200","300","400","500","600","700","800"],
+});
+
+export const metadata: Metadata = {
+  title: "EMOO",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="th">
+      <body className={k2d.className}>
+        {children}
+      </body>
+    </html>
+  );
+}

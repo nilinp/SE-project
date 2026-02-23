da0ff2a842157f8ad7bbe7f2f9bf73b6a2711861
gunikrengkidesu <chanunchidat6@gmail.com>
2026-02-23 20:34:18 +0700

    layout for shopping

---
+++ emoo/app/(public)/shopping/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { K2D } from "next/font/google";
+
+const k2d = K2D({
+  subsets: ["latin", "thai"],
+  weight: ["100","200","300","400","500","600","700","800"],
+});
+
+export const metadata: Metadata = {
+  title: "EMOO",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex-1 bg-(--main) min-h-screen">
+      {children}
+    </div>
+  );
+}

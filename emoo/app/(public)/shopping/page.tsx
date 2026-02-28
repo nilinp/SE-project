1c42544e1fd9a1ef3d5a6abb333ab4af2c9d34c4
Vanda <s6704062611034@email.kmutnb.ac.th>
2026-02-28 14:40:46 +0700

    update shopping page

---
+++ emoo/app/(public)/shopping/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function ShoppingPage() {
+    return (
+        <div className="min-h-screen bg-[#BCA48C] text-white flex items-center justify-center">
+            
+        </div>
+    );
+}

8e7535f229a4181703e81ed1e9a936790062a9a2
gunikrengkidesu <chanunchidat6@gmail.com>
2026-03-05 09:56:54 +0700

    install zustand

---
+++ emoo/lib/cartstore.ts
@@ -0,0 +1,51 @@
+import { create } from "zustand";
+
+type CartItem = {
+  id: string;
+  name: string;
+  price: number;
+  image: string;
+  quantity: number;
+};
+
+type CartStore = {
+  cart: CartItem[];
+  addToCart: (item: CartItem) => void;
+  increase: (id: string) => void;
+  decrease: (id: string) => void;
+};
+
+export const useCartStore = create<CartStore>((set) => ({
+  cart: [],
+
+  addToCart: (item) =>
+    set((state) => {
+      const exist = state.cart.find((p) => p.id === item.id);
+
+      if (exist) {
+        return {
+          cart: state.cart.map((p) =>
+            p.id === item.id ? { ...p, quantity: p.quantity + 1 } : p
+          ),
+        };
+      }
+
+      return {
+        cart: [...state.cart, { ...item, quantity: 1 }],
+      };
+    }),
+
+  increase: (id) =>
+    set((state) => ({
+      cart: state.cart.map((p) =>
+        p.id === id ? { ...p, quantity: p.quantity + 1 } : p
+      ),
+    })),
+
+  decrease: (id) =>
+    set((state) => ({
+      cart: state.cart.map((p) =>
+        p.id === id ? { ...p, quantity: p.quantity - 1 } : p
+      ),
+    })),
+}));
